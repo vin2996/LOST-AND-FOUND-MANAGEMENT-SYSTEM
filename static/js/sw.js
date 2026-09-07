6fdb53355fd19165bee4f82d5efdd82459a043e4
Alvin  Rey <vin.alvineballena@gmail.com>
2026-09-08 05:41:46 +0800

    Update sw.js

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,3 +1,10 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { self.clients.claim(); });
-self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).catch(()=> caches.match(e.request))) });
+const CACHE_NAME = "zdspgc-v1";
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", (e) => {
+  self.clients.claim();
+});
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
